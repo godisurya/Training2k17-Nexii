d485bc05b0a0f6c4080b0c231b635c8a46f440dd
godisurya <surya.1510@gmail.com>
2017-03-17 12:01:51 +0530

    Add files via upload

---
+++ menuselection.py
@@ -0,0 +1,15 @@
+while True:
+    print "1.MAC\n2.Linux\n3.Windows"
+    a = raw_input("Select One Option: ")
+    if a == "1":
+        print "MAC"
+    elif a == "2":
+        print "Linux"
+    elif a == "3":
+        print "Windows"
+    else:
+        print "Wrong selection"
+        break
+        
+    
+    
