fb18c6b541585d9349f37c5cfbf55abd11b31e00
godisurya <surya.1510@gmail.com>
2017-04-04 12:48:55 +0530

    Add files via upload

---
+++ evenorodd.py
@@ -0,0 +1,7 @@
+a = input("Enter the number you want to check whether even or odd: ")
+if a%2 == 0:
+    #print "The given  number {0} is : even".format(a)
+    print "The given number %d is even" %(a)
+else:
+    #print "The given number {0} is : odd".format(a)
+    print "The given number %d is odd" %(a)
---
+++ substringinstring.py
@@ -0,0 +1,7 @@
+a = raw_input("Enter the actual string: ")
+b = raw_input("Enter the substring: ")
+if b in a:
+    print "Substring is present in the actual string"
+
+else:
+    print "Substring is not found"
